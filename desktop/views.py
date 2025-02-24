9a0b75fb684349383d27f59467161e218481dfe2
HugoVitse <hugovitse1@gmail.com>
2025-02-24 13:41:20 +0100

    files update

--- desktop/views.py
+++ desktop/views.py
@@ -1,6 +1,11 @@
 # desktop/views.py
 from django.shortcuts import render
+from files.models import File
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def desktop_view(request):
     # Cette vue rendra le template desktop.html
-    return render(request, 'desktop/desktop.html')
+    files = File.objects.filter(user_id=request.user.id)  # Afficher uniquement les fichiers de l'utilisateur connecté
+    return render(request, 'desktop/desktop.html', {'files': files})
